d52d3bebdfb8da0b7fc7b4152a0fa89a71cb39f1
Javier Jure <jjure13@gmail.com>
2022-04-26 13:12:52 -0300

    Aprendiendo lazyload, basico

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes : Routes =[
+  {
+    path:'auth',
+    // Cuando alguien entre al path auth, recien carga el auth module, mediante la promesa
+    // Recien al entrar en auth hace el import del modulo, el buen lazy
+    loadChildren:()=>import('./auth/auth.module').then(m => m.AuthModule)
+  },{
+    path:'heroes',
+    loadChildren:()=> import('./heroes/heroes.module').then(m=> m.HeroesModule)
+  },
+  {path:'404',component:ErrorPageComponent},
+  {path:'**',redirectTo:'404'}
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
