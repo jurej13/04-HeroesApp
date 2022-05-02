3a28c1e4ebf3eb9cc7f37f404f8e7878a6f684ef
Javier Jure <jjure13@gmail.com>
2022-05-02 10:38:48 -0300

    Aplicacion terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes : Routes =[
@@ -7,10 +8,12 @@ const routes : Routes =[
     path:'auth',
     // Cuando alguien entre al path auth, recien carga el auth module, mediante la promesa
     // Recien al entrar en auth hace el import del modulo, el buen lazy
-    loadChildren:()=>import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren:()=>import('./auth/auth.module').then(m => m.AuthModule),
   },{
     path:'heroes',
-    loadChildren:()=> import('./heroes/heroes.module').then(m=> m.HeroesModule)
+    loadChildren:()=> import('./heroes/heroes.module').then(m=> m.HeroesModule),
+    canLoad:[AuthGuard],
+    canActivate:[AuthGuard]
   },
   {path:'404',component:ErrorPageComponent},
   {path:'**',redirectTo:'404'}
